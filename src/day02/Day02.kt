8aad92fc8bb1cbb116356cbd50f852c8474a924a
Patrik Bjurling <3659683+PKBJ@users.noreply.github.com>
2024-12-02 07:58:22 +0100

    Solutions for day 2

---
+++ src/day02/Day02.kt
@@ -0,0 +1,65 @@
+package day02
+
+import readInput
+import kotlin.math.abs
+
+fun main() {
+    fun part1(input: List<String>): Int = countSafeReports(input)
+    fun part2(input: List<String>): Int = countSafeReportsIfToleratingOneBadLevel(input)
+
+    val testInputPart1 = readInput(name = "Day02_test_part", "day02")
+    check(part1(testInputPart1) == 2)
+    val testInputPart2 = readInput(name = "Day02_test_part", "day02")
+    check(part2(testInputPart2) == 4)
+
+    val input = readInput(name = "Day02", "day02")
+    println("=== Part 1 ===")
+    println(part1(input))
+    println("=== Part 2 ===")
+    println(part2(input))
+}
+
+private fun List<String>.reports(): List<List<Int>> = map { line ->
+    line
+        .split(" ")
+        .map { it.toInt() }
+}
+
+private fun countSafeReports(input: List<String>): Int =
+    input
+        .reports()
+        .count { it.isSafeReport() }
+
+private fun countSafeReportsIfToleratingOneBadLevel(input: List<String>): Int =
+    input
+        .reports()
+        .count { report ->
+            var isSafeReport = report.isSafeReport()
+            if (!isSafeReport) {
+                for (i in report.indices) {
+                    val modifiedReport = report.take(i) + report.takeLast(report.lastIndex - i)
+                    if (modifiedReport.isSafeReport()) {
+                        isSafeReport = true
+                        break
+                    }
+                }
+            }
+            isSafeReport
+        }
+
+private fun List<Int>.isSafeReport(): Boolean =
+    when {
+        distinct() != this -> false
+        sorted() != this && sortedDescending() != this -> false
+        else -> {
+            var isSafe = true
+            for (i in 0..<lastIndex) {
+                if (abs(this[i] - this[i + 1]) !in 1..3) {
+                    isSafe = false
+                    break
+                }
+            }
+            isSafe
+        }
+    }
+
